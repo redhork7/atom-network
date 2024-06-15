96e4bcfed129b3650c92474e6d486869a351b451
redhork7 <redhork7@redhork7.com>
2024-06-15 16:31:37 +0900

    atom-network: module 부 추가

--- apps/atom-network/src/app.module.ts
+++ apps/atom-network/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { resolve } from 'path';
+import { validate as validateDatabaseEnv } from '@app/database/env.validate';
+import { validate as validateAuthEnv } from '@app/auth/env.validate';
+import { AccountsModule } from './accounts/accounts.module';
+import { DevicesModule } from './devices/devices.module';
+import { SignModule } from './sign/sign.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: resolve(__dirname, '../../../../../../.env.atom-network'),
+      validate: (config) => {
+        return validateDatabaseEnv(config) && validateAuthEnv(config);
+      },
+    }),
+    DevicesModule,
+    AccountsModule,
+    SignModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
