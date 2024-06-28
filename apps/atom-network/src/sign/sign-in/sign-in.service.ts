24ca3c54c576b7c2bab16009b1899e806b85d83c
redhork7 <redhork7@redhork7.com>
2024-06-28 14:18:58 +0900

    atom-network: 의미없는 형변환 제거

--- apps/atom-network/src/sign/sign-in/sign-in.service.ts
+++ apps/atom-network/src/sign/sign-in/sign-in.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { AccountsService } from '../../accounts/accounts.service';
 import { ISignInWithAccountDto } from '../sign.dto';
-import { IAccountsFindUidByDto } from '../../accounts/accounts.dto';
 
 @Injectable()
 export class SignInService {
   constructor(private readonly accountsService: AccountsService) {}
 
   async withAccount(dto: ISignInWithAccountDto): Promise<number> {
-    return await this.accountsService.findUidBy(dto as IAccountsFindUidByDto);
+    return await this.accountsService.findUidBy(dto);
   }
 }
--- apps/atom-network/src/sign/sign-up/sign-up.service.ts
+++ apps/atom-network/src/sign/sign-up/sign-up.service.ts
@@ -1,16 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { AccountsService } from '../../accounts/accounts.service';
 import { ISignUpWithAccountDto } from '../sign.dto';
-import { IAccountsRegisterDto } from '../../accounts/accounts.dto';
 
 @Injectable()
 export class SignUpService {
   constructor(private readonly accountsService: AccountsService) {}
 
   async withAccount(dto: ISignUpWithAccountDto): Promise<number> {
-    const uid = await this.accountsService.register(
-      dto as IAccountsRegisterDto,
-    );
+    const uid = await this.accountsService.register(dto);
 
     return uid;
   }
