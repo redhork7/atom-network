803b3d52c95afc335fc2052cb1ba72c93ec3df95
redhork7 <redhork7@redhork7.com>
2024-07-10 19:18:12 +0900

    chefrigo: msa 구성

--- apps/chefrigo/src/main.ts
+++ apps/chefrigo/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { ChefrigoModule } from './chefrigo.module';
+import { ConfigService } from '@nestjs/config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(ChefrigoModule);
-  await app.listen(3000);
+  const appConfig = app.get<ConfigService>(ConfigService);
+  const appPort = appConfig.get<number>('APP_PORT_CHEFRIGO');
+  const msa = await NestFactory.createMicroservice<MicroserviceOptions>(
+    ChefrigoModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '0.0.0.0',
+        port: appPort,
+      },
+    },
+  );
+
+  await msa.listen();
+  await app.listen(+appPort + 1);
 }
 bootstrap();
